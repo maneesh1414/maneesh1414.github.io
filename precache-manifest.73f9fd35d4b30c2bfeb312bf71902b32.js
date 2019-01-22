4dfd423233de2f925fc58ef2e01b5569a30d41f0
venkateshmareddy <maneeshraja1414@gmail.com>
2019-01-22 22:15:46 +0530

    hosting the application

---
+++ precache-manifest.73f9fd35d4b30c2bfeb312bf71902b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c94195fa07c0874ca9ca3c544103c53",
+    "url": "./static/media/content.5c94195f.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9de74ad4f099a996421c",
+    "url": "./static/js/main.9de74ad4.chunk.js"
+  },
+  {
+    "revision": "51e409109db2f2665fdd",
+    "url": "./static/js/1.51e40910.chunk.js"
+  },
+  {
+    "revision": "9de74ad4f099a996421c",
+    "url": "./static/css/main.8f9b90e9.chunk.css"
+  },
+  {
+    "revision": "346da3ae05b8a41cd213538b23b3fe43",
+    "url": "./index.html"
+  }
+];
